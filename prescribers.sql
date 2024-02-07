095865e36ee13434678eb83751126baf2f6f4337
Teresa Whitesell <tfwhitesell@gmail.com>
2024-02-06 20:52:32 -0600

    question 12

--- prescribers.sql
+++ prescribers.sql
@@ -8,10 +8,22 @@ ON p1.npi = p2.npi
 GROUP BY 1, 2, 3
 ORDER BY 4 DESC
 LIMIT 1;
--- Bruce Pendley had the highest number of claims across all drugs.
+-- Bruce Pendley (npi 1881634483) had the highest number of claims across all drugs.
     
 --     b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name, 
 -- 	specialty_description, and the total number of claims.
+SELECT p1.npi, 
+	p2.nppes_provider_first_name, 
+	p2.nppes_provider_last_org_name, 
+	p2.specialty_description, 
+	SUM(total_claim_count) AS total_claims
+FROM prescription AS p1
+LEFT JOIN prescriber AS p2
+ON p1.npi = p2.npi
+GROUP BY 1, 2, 3, 4
+ORDER BY 5 DESC
+LIMIT 1;
+-- Bruce Pendley's specialty description is Family Practice.
 
 -- 2. 
 --     a. Which specialty had the most total number of claims (totaled over all drugs)?
