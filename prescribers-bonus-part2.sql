dc6ef50e34cd6211102c44499543c0e841108d6e
Teresa Whitesell <tfwhitesell@gmail.com>
2024-02-10 11:49:12 -0600

    bonus part 2 question 2a and 2b

--- prescribers-bonus-part2.sql
+++ prescribers-bonus-part2.sql
@@ -31,11 +31,46 @@ WHERE npi NOT IN (
 
 -- 2.
 --     a. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Family Practice.
+SELECT generic_name,
+	SUM(total_claim_count) AS total_claims
+FROM drug AS d
+INNER JOIN prescription AS p1
+	ON d.drug_name = p1.drug_name
+INNER JOIN prescriber AS p2
+	ON p1.npi = p2.npi
+WHERE specialty_description = 'Family Practice'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
 
 --     b. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Cardiology.
+SELECT generic_name,
+	SUM(total_claim_count) AS total_claims
+FROM drug AS d
+INNER JOIN prescription AS p1
+	ON d.drug_name = p1.drug_name
+INNER JOIN prescriber AS p2
+	ON p1.npi = p2.npi
+WHERE specialty_description = 'Cardiology'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
 
 --     c. Which drugs are in the top five prescribed by Family Practice prescribers and Cardiologists? 
 -- 	Combine what you did for parts a and b into a single query to answer this question.
+SELECT specialty_description,
+	generic_name,
+	SUM(total_claim_count) AS total_claims
+FROM drug AS d
+INNER JOIN prescription AS p1
+	ON d.drug_name = p1.drug_name
+INNER JOIN prescriber AS p2
+	ON p1.npi = p2.npi
+WHERE specialty_description IN ('Cardiology', 'Family Practice')
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 5;
+-- not sure this is the intended answer, try again with a window function
 
 -- 3. Your goal in this question is to generate a list of the top prescribers in each of the major metropolitan areas of Tennessee.
 --     a. First, write a query that finds the top 5 prescribers in Nashville in terms of the total number of claims (total_claim_count) 
